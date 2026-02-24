bf8b543a7da715557778230b0ded8b06127af8e2
PigeonBar <56974298+PigeonBar@users.noreply.github.com>
2026-02-23 22:01:05 -0500

    v1 of test script

---
+++ test.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Flower - Update Test
+// @namespace    http://tampermonkey.net/
+// @version      2026-02-23
+// @description  I am testing Tampermonkey's ability to detect updates for scripts posted on Github.
+// @author       PigeonBar
+// @match        https://flowr.fun/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=flowr.fun
+// @grant        none
+// ==/UserScript==
+
+console.log("Hello, World!");
