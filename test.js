da695da4ba38d12a60f975434d983b63bb69f4fb
PigeonBar <56974298+PigeonBar@users.noreply.github.com>
2026-02-23 22:02:17 -0500

    Add URL to check for updates

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         Flower - Update Test
+// @name         Flowr - Update Test
 // @namespace    http://tampermonkey.net/
 // @version      2026-02-23
-// @description  I am testing Tampermonkey's ability to detect updates for scripts posted on Github.
+// @description  I am testing Tampermonkey's ability to detect updates for userscripts posted on Github.
 // @author       PigeonBar
 // @match        https://flowr.fun/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=flowr.fun
+// @updateURL    https://raw.githubusercontent.com/PigeonBar/test-script/refs/heads/main/test.js
 // @grant        none
 // ==/UserScript==
 
